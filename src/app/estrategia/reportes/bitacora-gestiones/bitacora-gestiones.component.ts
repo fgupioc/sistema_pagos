5ad9b1f46ce76a6d17b8a023fe6c48b446c866cc
franz gupioc <franz.gupioc@cognitivesoft.tech>
2021-01-18 16:34:04 -0500

    mejora

--- src/app/estrategia/reportes/bitacora-gestiones/bitacora-gestiones.component.ts
+++ src/app/estrategia/reportes/bitacora-gestiones/bitacora-gestiones.component.ts
@@ -7,6 +7,7 @@ import { GestorMoneda } from 'src/app/interfaces/reportes/bitacora-gestiones/ges
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { TreeviewItem, TreeviewConfig } from 'ngx-treeview';
 
 @Component({
   selector: 'app-bitacora-gestiones',
@@ -30,11 +31,35 @@ export class BitacoraGestionesComponent implements OnInit {
   totalGestionesNoRealizadasDolar = 0;
   totalGestionesNoRealizadasSoles: any;
 
+  dropdownEnabled = true;
+  items: TreeviewItem[];
+  values: number[];
+  config = TreeviewConfig.create({
+    hasAllCheckBox: true,
+    hasFilter: true,
+    hasCollapseExpand: true,
+    decoupleChildFromParent: false,
+    maxHeight: 400
+  });
+
+  buttonClasses = [
+    'btn-outline-primary',
+    'btn-outline-secondary',
+    'btn-outline-success',
+    'btn-outline-danger',
+    'btn-outline-warning',
+    'btn-outline-info',
+    'btn-outline-light',
+    'btn-outline-dark'
+  ];
+  buttonClass = this.buttonClasses[0];
+
   constructor(
     private reportesService: ReportesService,
     private spinner: NgxSpinnerService,
     private formBuilder: FormBuilder
   ) {
+    this.items = this.getBooks();
   }
 
   ngOnInit() {
@@ -80,4 +105,50 @@ export class BitacoraGestionesComponent implements OnInit {
     this.loadList(start, finish);
   }
 
+  onFilterChange(value: string): void {
+    console.log('filter:', value);
+  }
+
+  getBooks(): TreeviewItem[] {
+    const childrenCategory = new TreeviewItem({
+      text: 'Children', value: 1, collapsed: true, children: [
+        { text: 'Baby 3-5', value: 11 },
+        { text: 'Baby 6-8', value: 12 },
+        { text: 'Baby 9-12', value: 13 }
+      ]
+    });
+    const itCategory = new TreeviewItem({
+      text: 'IT', value: 9, children: [
+        {
+          text: 'Programming', value: 91, children: [{
+            text: 'Frontend', value: 911, children: [
+              { text: 'Angular 1', value: 9111 },
+              { text: 'Angular 2', value: 9112 },
+              { text: 'ReactJS', value: 9113, disabled: true }
+            ]
+          }, {
+            text: 'Backend', value: 912, children: [
+              { text: 'C#', value: 9121 },
+              { text: 'Java', value: 9122 },
+              { text: 'Python', value: 9123, checked: false, disabled: true }
+            ]
+          }]
+        },
+        {
+          text: 'Networking', value: 92, children: [
+            { text: 'Internet', value: 921 },
+            { text: 'Security', value: 922 }
+          ]
+        }
+      ]
+    });
+    const teenCategory = new TreeviewItem({
+      text: 'Teen', value: 2, collapsed: true, disabled: true, children: [
+        { text: 'Adventure', value: 21 },
+        { text: 'Science', value: 22 }
+      ]
+    });
+    const othersCategory = new TreeviewItem({ text: 'Others', value: 3, checked: false, disabled: true });
+    return [childrenCategory, itCategory, teenCategory, othersCategory];
+  }
 }
